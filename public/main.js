f13b7c677419e0a3d31a8757ae732e23e636cd8b
Justin Marin <jumarin@protonmail.com>
2021-05-20 14:29:38 -0400

    Explorer mode complete

--- public/main.js
+++ public/main.js
@@ -1,6 +1,140 @@
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  const rock = 'rock'
+  const paper = 'paper'
+  const scissors = 'scissors'
+  const lizard = 'lizard'
+  const spock = 'spock'
+  const yourTurn = 'Please make your choice'
+  const choiceMade = 'Choice Made'
+  const win = 'You win!'
+  const lose = 'You lose :('
+  const draw = 'Draw!'
+
+  let oneChoice = 'one'
+  let twoChoice = 'two'
+  let oneDisplay = document.querySelector('.one p')
+  let twoDisplay = document.querySelector('.two p')
+
+  const oneRock = document
+    .querySelector('.one button.rock')
+    .addEventListener('click', pickOneRock)
+
+  function pickOneRock() {
+    oneChoice = rock
+    oneDisplay.textContent = choiceMade
+    twoDisplay.textContent = yourTurn
+  }
+
+  const onePaper = document
+    .querySelector('.one button.paper')
+    .addEventListener('click', pickOnePaper)
+
+  function pickOnePaper() {
+    oneChoice = paper
+    oneDisplay.textContent = choiceMade
+    twoDisplay.textContent = yourTurn
+  }
+
+  const oneScissors = document
+    .querySelector('.one button.scissors')
+    .addEventListener('click', pickOneScissors)
+
+  function pickOneScissors() {
+    oneChoice = scissors
+    oneDisplay.textContent = choiceMade
+    twoDisplay.textContent = yourTurn
+  }
+
+  const oneLizard = document
+    .querySelector('.one button.lizard')
+    .addEventListener('click', pickOneLizard)
+
+  function pickOneLizard() {
+    oneChoice = lizard
+    oneDisplay.textContent = choiceMade
+    twoDisplay.textContent = yourTurn
+  }
+
+  const oneSpock = document
+    .querySelector('.one button.spock')
+    .addEventListener('click', pickOneSpock)
+
+  function pickOneSpock() {
+    oneChoice = spock
+    oneDisplay.textContent = choiceMade
+    twoDisplay.textContent = yourTurn
+  }
+
+  const twoRock = document
+    .querySelector('.two button.rock')
+    .addEventListener('click', picktwoRock)
+
+  function picktwoRock() {
+    twoChoice = rock
+    showWinner()
+  }
+
+  const twoPaper = document
+    .querySelector('.two button.paper')
+    .addEventListener('click', picktwoPaper)
+
+  function picktwoPaper() {
+    twoChoice = paper
+    showWinner()
+  }
+
+  const twoScissors = document
+    .querySelector('.two button.scissors')
+    .addEventListener('click', picktwoScissors)
+
+  function picktwoScissors() {
+    twoChoice = scissors
+    showWinner()
+  }
+
+  const twoLizard = document
+    .querySelector('.two button.lizard')
+    .addEventListener('click', pickTwoLizard)
+
+  function pickTwoLizard() {
+    twoChoice = lizard
+    showWinner()
+  }
+
+  const twoSpock = document
+    .querySelector('.two button.spock')
+    .addEventListener('click', pickTwoSpock)
+
+  function pickTwoSpock() {
+    twoChoice = spock
+    showWinner()
+  }
+
+  function showWinner() {
+    if (oneChoice === twoChoice) {
+      oneDisplay.textContent = draw
+      twoDisplay.textContent = draw
+    } else if (
+      (oneChoice === rock && (twoChoice === scissors || twoChoice == lizard)) ||
+      (oneChoice === paper && (twoChoice === rock || twoChoice == spock)) ||
+      (oneChoice === scissors &&
+        (twoChoice === paper || twoChoice === lizard)) ||
+      (oneChoice === lizard && (twoChoice === spock || twoChoice === paper)) ||
+      (oneChoice === spock && (twoChoice === scissors || twoChoice === rock))
+    ) {
+      oneDisplay.textContent = win
+      twoDisplay.textContent = lose
+    } else if (
+      (twoChoice === rock && (oneChoice === scissors || oneChoice == lizard)) ||
+      (twoChoice === paper && (oneChoice === rock || oneChoice == spock)) ||
+      (twoChoice === scissors &&
+        (oneChoice === paper || oneChoice === lizard)) ||
+      (twoChoice === lizard && (oneChoice === spock || oneChoice === paper)) ||
+      (twoChoice === spock && (oneChoice === scissors || oneChoice === rock))
+    ) {
+      oneDisplay.textContent = lose
+      twoDisplay.textContent = win
+    }
   }
 }
 
